614753d8597f418c848e191d0291487fc6e5bc61
Mahyar Mirrashed <mah.mirr@gmail.com>
2021-06-25 14:37:28 -0500

    Fix spacing

--- src/interfaces/NominationStorage.ts
+++ src/interfaces/NominationStorage.ts
@@ -3,5 +3,5 @@ import { Snowflake } from 'discord.js';
 export default interface Nomination {
 	week: string;
 	nominations: { nominator: Snowflake; nominee: Snowflake; reason: string }[];
-    message: Snowflake;
+	message: Snowflake;
 }
