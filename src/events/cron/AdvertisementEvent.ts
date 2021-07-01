efd5f0905fd9e654e86e91abb2d7d5bc82914d9f
Mahyar Mirrashed <mah.mirr@gmail.com>
2021-07-01 02:35:44 -0500

    Update advertisement API request

--- src/events/cron/AdvertisementEvent.ts
+++ src/events/cron/AdvertisementEvent.ts
@@ -4,7 +4,6 @@ import TurndownService from 'turndown';
 import Bot from '../../client/Client';
 import RunFunction from '../../interfaces/RunFunctionStorage';
 import Advertisement from '../../types/AdvertisementType';
-import moment from 'moment';
 
 // mappings to escape HTML-formatted results from vTools Events API
 const MAPPINGS = Object.assign(Object.create(null), {
@@ -21,50 +20,47 @@ const MAPPINGS = Object.assign(Object.create(null), {
 export const run: RunFunction = async (client: Bot): Promise<void> => {
 	axios
 		.get(
-			// generate time delta for past seven days
-			`https://services13.ieee.org/RST/events/api/public/v2/events/list.json?span=${moment()
-				.day(-7)
-				.toISOString()}~${moment().day(0).toISOString()}`
+			// fetch all upcoming events
+			'https://events.vtools.ieee.org/feeds/v2/c/R70031?span=now~&sort=start_time'
 		)
 		.then((res: AxiosResponse) => {
 			const {
 				data: { data: advertisements },
 			}: { data: { data: Advertisement[] } } = res;
 
-			if (client.guild) {
+			if (client.guild && advertisements.length > 0) {
 				// find 'events' channel
 				const channel: TextChannel = client.guild.channels.cache.find(
 					(channel: GuildChannel) =>
 						channel.name === 'events' && channel instanceof TextChannel
 				) as TextChannel;
 
-				advertisements
-					// filter to only Winnipeg results
-					.filter(
-						(advertisement: Advertisement) =>
-							advertisement.attributes.city === 'Winnipeg'
-					)
-					// post each filtered result as embed to 'events' channel
-					.forEach((advertisement: Advertisement) =>
-						client.sendEmbed(channel, {
-							author: { name: 'Winnipeg Section Event Announcement!' },
-							title: advertisement.attributes.title,
-							// use TurndownService to convert HTML to Markdown
-							description: new TurndownService().turndown(
-								advertisement.attributes.description.replace(
-									/\\(["'\\bfnrt])/g,
-									(_, c) => `${MAPPINGS[c] || c}`
-								)
-							),
-							fields: [
-								{
-									name: 'Registration Link:',
-									value: advertisement.attributes.link,
-									inline: true,
-								},
-							],
-						})
-					);
+				// header for advertisements
+				client.sendEmbed(channel, {
+					title: '**Upcoming Events in IEEE Winnipeg Section**',
+				});
+
+				// post each advertisement as embed to 'events' channel
+				advertisements.forEach((advertisement: Advertisement) =>
+					client.sendEmbed(channel, {
+						author: { name: 'Winnipeg Section Event Announcement!' },
+						title: advertisement.attributes.title,
+						// use TurndownService to convert HTML to Markdown
+						description: new TurndownService().turndown(
+							advertisement.attributes.description.replace(
+								/\\(["'\\bfnrt])/g,
+								(_, c) => `${MAPPINGS[c] || c}`
+							)
+						),
+						fields: [
+							{
+								name: 'Registration Link:',
+								value: advertisement.attributes.link,
+								inline: true,
+							},
+						],
+					})
+				);
 			}
 		})
 		.catch((e: unknown) => client.logger.error(e));
