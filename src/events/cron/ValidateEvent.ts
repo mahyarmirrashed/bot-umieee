db06ced0e1705c69cf9e3c07357a47eb5886c166
Mahyar Mirrashed <mah.mirr@gmail.com>
2021-09-05 17:03:12 -0500

    feat: add user pruning based on inactivity period
    Based on how long the user has remained inactive on the server, prune (kick) them. Otherwise, if the
    user is not inactive, potentially remove them from the inactive user database.

--- src/events/cron/ValidateEvent.ts
+++ src/events/cron/ValidateEvent.ts
@@ -1,6 +1,10 @@
+import { Guild, GuildMember } from 'discord.js';
 import Bot from '../../client/Client';
+import findOrCreateInactiveRole from '../../helpers/guild/FindOrCreateInactiveRole';
 import generateToken from '../../helpers/membership/GenerateToken';
 import modifyMember from '../../helpers/membership/ModifyMember';
+import remindOrKickInactiveMember from '../../helpers/membership/RemindOrKickInactiveUser';
+import removeInactiveUser from '../../helpers/membership/RemoveInactiveUser';
 import validateMember from '../../helpers/membership/ValidateMember';
 import Handler from '../../interfaces/HandlerStorage';
 import MembershipModel from '../../models/MembershipModel';
@@ -10,26 +14,37 @@ import Membership from '../../types/MembershipType';
 export const cronJobFrequency = '0 0 * * *';
 
 export const handler: Handler<unknown> = async (client: Bot): Promise<void> => {
-  generateToken().then(async (token: string) => {
-    ((await MembershipModel.find({}).exec()) as Membership[]).forEach(
-      async (membership: Membership) => {
-        const guild = await client.guilds.fetch(membership.guildID);
-        // type guard for possibly null guild
-        if (guild) {
-          const member = await guild.members.fetch(membership.userID);
-          // type guard for possibly null member
-          if (member) {
-            // finally, perform member role modification
-            modifyMember(
-              guild,
-              member,
-              await validateMember(token, membership.ieeeID),
-            );
+  generateToken()
+    .then(async (token: string) => {
+      ((await MembershipModel.find({}).exec()) as Membership[]).forEach(
+        async (membership: Membership) => {
+          const guild = await client.guilds.fetch(membership.guildID);
+          // type guard for possibly null guild
+          if (guild) {
+            const member = await guild.members.fetch(membership.userID);
+            // type guard for possibly null member
+            if (member) {
+              const valid = await validateMember(token, membership.ieeeID);
+              // perform member role modification
+              modifyMember(guild, member, valid);
+            }
           }
-        }
-      },
-    );
-  });
+        },
+      );
+    })
+    .then(() => {
+      client.guilds.cache.forEach(async (guild: Guild) => {
+        const inactiveRole = await findOrCreateInactiveRole(guild);
+        guild.members.cache.forEach((member: GuildMember) => {
+          if (member.roles.cache.has(inactiveRole.id)) {
+            remindOrKickInactiveMember(client, guild, member);
+          } else {
+            // potentially remove user from inactive user database
+            removeInactiveUser(client, guild, member);
+          }
+        });
+      });
+    });
 };
 
 export const name = 'validate';
