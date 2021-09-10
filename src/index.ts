bdbf3dc3c11b30a0bc1caf7d1fcd19f25427f219
Mahyar Mirrashed <mah.mirr@gmail.com>
2021-09-10 01:38:50 -0500

    fix(index): add check for environment variables
    Add conditional statement to ensure existence of necessary environment variables.

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,19 @@ import Bot from './client/Client';
 // set up environment variables from .env file
 config();
 
-new Bot().start();
+// ensure environment variables are set
+if (
+  [
+    process.env.CLIENT_ID,
+    process.env.DATABASE_URI,
+    process.env.DISCORD_TOKEN,
+    process.env.GUILD_ID,
+    process.env.MEMBERSHIP_VALIDATOR_CLIENT_ID,
+    process.env.MEMBERSHIP_VALIDATOR_CLIENT_SECRET,
+  ].every((envVar: string | undefined) => envVar)
+) {
+  new Bot().start();
+} else {
+  // eslint-disable-next-line no-console
+  console.error('One or more environment variables are not set.');
+}
