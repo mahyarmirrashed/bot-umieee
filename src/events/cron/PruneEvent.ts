b58d79dcb991fa140c23173ce20ff3edd6747fa3
Mahyar Mirrashed <mah.mirr@gmail.com>
2021-09-10 01:45:11 -0500

    feat: add feature to prune old members
    Prunes members who have been inactive for more than four weeks.

---
+++ src/events/cron/PruneEvent.ts
@@ -0,0 +1,22 @@
+import { Guild } from 'discord.js';
+import Bot from '../../client/Client';
+import Handler from '../../interfaces/HandlerStorage';
+
+export const cronJobFrequency = '0 0 * * MON';
+
+export const handler: Handler<unknown> = async (client: Bot): Promise<void> => {
+  // prune all guilds
+  await client.guilds.fetch();
+  client.guilds.cache.each(async (guild: Guild) => {
+    guild.members
+      .prune({
+        days: 28,
+        reason: 'Please rejoin when you are active on the server again.',
+      })
+      .then((pruneCount: number) =>
+        client.logger.info(`Pruned ${pruneCount} members on ${guild.id}`),
+      );
+  });
+};
+
+export const name = 'prune';
